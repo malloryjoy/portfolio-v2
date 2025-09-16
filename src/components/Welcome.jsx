d3a83887e8196d961d34b0e1e22ade6723a8e143
Mallory Joy <malloryjoy13@gmail.com>
2025-09-16 14:02:27 -0400

    Re-proportioned text

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -10,7 +10,7 @@ class Welcome extends React.Component {
            <div aria-labelledby="Mallory Joy">
            <h1 className="font-mogra text-shadow-xl text-xl">MALLORY JOY</h1>
            <p className="leading-1.5 text-base py-8">EST. 1994</p>
-           <h2 className="mt-6 text-center font-vidaloka font-extrabold text-5xl">FEARLESS AMBITION. <span id="smo" className="ml-65">FLAWLESS WORK.</span></h2>
+           <h2 className="mt-6 text-center font-vidaloka font-extrabold text-5xl flex justify-between items-center px-4 sm:px-6 md:px-8"><span>FEARLESS AMBITION. </span><span>FLAWLESS WORK.</span></h2>
            <div className="mt-40 pb-22 mb-22 font-extrabold text-3xl text-black"><p><a aria-label="Scroll down to about me section" href="#aboutme"><i class="fa-solid fa-chevron-down"></i></a></p></div>
        </div>
         </section>
